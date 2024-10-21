6e5e98192f7c020125dbf8608a7c39af4cf3f803
ogava_2325 <dimapajuk2325@gmail.com>
2024-10-21 14:41:21 +0300

    Fixed error with Task namespace

--- TaskManagement.Domain/Entities/Task.cs
+++ TaskManagement.Domain/Entities/Task.cs
@@ -1,14 +1,13 @@
 using TaskManagement.Domain.Enums;
-using TaskStatus = System.Threading.Tasks.TaskStatus;
 
 namespace TaskManagement.Domain.Entities;
 
 public class Task : AuditableEntity
 {
-    public string Title { get; private set; }
-    public string Description { get; private set; }
-    public DateTime? DueDate { get; private set; }
-    public TaskStatus Status { get; private set; }
-    public TaskPriority Priority { get; private set; }
-    public Guid UserId { get; private set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime? DueDate { get; set; }
+    public Status Status { get; set; }
+    public Priority Priority { get; set; }
+    public Guid UserId { get; set; }
 }
